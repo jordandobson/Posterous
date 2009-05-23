9d647fd96bbc8cdbd26cda159f025e0d5c9d2ad5
Jordan Dobson <jordan.dobson@madebysquad.com>
2009-05-22 17:21:04 -0700

    Added stubs for ping_account & valid_user?

--- test/test_posterous.rb
+++ test/test_posterous.rb
@@ -1,29 +1,28 @@
 require 'test/unit'
 require 'posterous'
 require 'mocha'
+require 'fakeweb'
 
 class TestPosterous < Test::Unit::TestCase
 
+  FakeWeb.allow_net_connect = false
+
   def setup
-    @valid_resp   = {"site"=>{"name"=>"ruby-posterous's posterous", "primary"=>"true", "private"=>"false", "url"=>"http://ruby-posterous.posterous.com", "id"=>"174966"}, "stat"=>"ok"}
-    @invalid_resp = {"err"=>{"msg"=>"Invalid Posterous email or password", "code"=>"3001"}, "stat"=>"fail"}
+    @resp_ok = {"rsp"=>{"site"=>{"name"=>"ruby-posterous's posterous", "primary"=>"true", "private"=>"false", "url"=>"http://ruby-posterous.posterous.com", "id"=>"174966"}, "stat"=>"ok"}}
+    @resp_fail = {"rsp"=>{"err"=>{"msg"=>"Invalid Posterous email or password", "code"=>"3001"}, "stat"=>"fail"}}
+    @resp_ok_two_sites = {"rsp"=>{"site"=>[{"name"=>"ruby-posterous's posterous", "primary"=>"true", "private"=>"false", "url"=>"http://ruby-posterous.posterous.com", "id"=>"174966"}, {"name"=>"uw-ruby", "primary"=>"false", "private"=>"false", "url"=>"http://uwruby.posterous.com", "id"=>"175260"}], "stat"=>"ok"}}
+
   end
 
   def test_raises_if_username_is_blank
     assert_raise PosterousAuthError do
       Posterous.new('', 'password')
     end
-  end
+  end  
 
   def test_raises_if_password_is_blank
     assert_raise PosterousAuthError do
       Posterous.new('email_address', '')
-    end
-  end
-  
-  def test_raises_if_username_is_not_srting
-    assert_raise PosterousAuthError do
-      Posterous.new(666, 'password')
     end
   end
   
@@ -32,7 +31,13 @@ class TestPosterous < Test::Unit::TestCase
       Posterous.new('email_address', 666)
     end
   end
-
+    
+  def test_raises_if_username_is_not_srting
+    assert_raise PosterousAuthError do
+      Posterous.new(666, 'password')
+    end
+  end
+  
   def test_site_id_is_false
     actual = Posterous.new('email_address', 'password')
     assert_equal false, actual.site_id
@@ -46,24 +51,47 @@ class TestPosterous < Test::Unit::TestCase
   def test_site_id_is_converted_to_string
     actual = Posterous.new('email_address', 'password', 174966)
     assert_equal '174966', actual.site_id
-  end
+  end  
 
   def test_user_authentication_success
-    post = Posterous.new('jordandobson@gmail.com', 'password')
-    post.stubs(:ping_account).returns(@valid_resp)
+    post = Posterous.new('email_address', 'password')
+    post.stubs(:ping_account).returns(@resp_ok)
     assert_equal true, post.valid_user?
   end
-
+  
   def test_user_authentication_fail
-    post = Posterous.new('failed@account.com', 'password')
-    post.stubs(:ping_account).returns(@invalid_resp)
+    post = Posterous.new('bad_email', 'password')
+    post.stubs(:ping_account).returns(@resp_fail)
     assert_equal false, post.valid_user?
   end
+
+  def test_ping_authentication_response
+    Posterous.stubs(:post).returns(@resp_ok)
+    assert_equal @resp_ok, Posterous.new('jordandobson@gmail.com', 'password').ping_account
+  end
   
+  def test_user_authentication_fail
+    Posterous.stubs(:post).returns(@resp_fail)
+    assert_equal @resp_fail, Posterous.new('jordandobson@gmail.com', 'password').ping_account
+  end
+
+end
+
+#   def test_traditional_mocking
+#     object = mock()
+#     object.expects(:expected_method).with(:p1, :p2).returns(:result)
+#     assert_equal :result, object.expected_method(:p1, :p2)
+#   end
 #   def test_user_has_site
 #     # works fine without fake web
 #     actual = Posterous.new('jordandobson', 'password', 175260)
 #     assert actual.has_site?
 #   end
 
-end
+
+
+
+
+
+
+
