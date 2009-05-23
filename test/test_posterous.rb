1568e660c90db7b8f4210fe98b33ab4535b2b3d4
Jordan Dobson <jordan.dobson@madebysquad.com>
2009-05-22 18:32:16 -0700

    Updated tests to be learner

--- test/test_posterous.rb
+++ test/test_posterous.rb
@@ -9,6 +9,9 @@ class TestPosterous < Test::Unit::TestCase
   FakeWeb.allow_net_connect = false
 
   def setup
+    @e = "email"
+    @p = "password"
+    @new_obj = Posterous.new(@e, @p)
     @resp_ok = {"rsp"=>{"site"=>{"name"=>"ruby-posterous's posterous", "primary"=>"true", "private"=>"false", "url"=>"http://ruby-posterous.posterous.com", "id"=>"174966"}, "stat"=>"ok"}}
     @resp_fail = {"rsp"=>{"err"=>{"msg"=>"Invalid Posterous email or password", "code"=>"3001"}, "stat"=>"fail"}}
     @resp_ok_two_sites = {"rsp"=>{"site"=>[{"name"=>"ruby-posterous's posterous", "primary"=>"true", "private"=>"false", "url"=>"http://ruby-posterous.posterous.com", "id"=>"174966"}, {"name"=>"uw-ruby", "primary"=>"false", "private"=>"false", "url"=>"http://uwruby.posterous.com", "id"=>"175260"}], "stat"=>"ok"}}
@@ -18,65 +21,61 @@ class TestPosterous < Test::Unit::TestCase
 
   def test_raises_if_username_is_blank
     assert_raise PosterousAuthError do
-      Posterous.new('', 'password')
+      Posterous.new('', @p)
     end
   end  
 
   def test_raises_if_password_is_blank
     assert_raise PosterousAuthError do
-      Posterous.new('email_address', '')
+      Posterous.new(@e, '')
     end
   end
   
   def test_raises_if_password_is_not_srting
     assert_raise PosterousAuthError do
-      Posterous.new('email_address', 666)
+      Posterous.new(@e, 666)
     end
   end
     
   def test_raises_if_username_is_not_srting
     assert_raise PosterousAuthError do
-      Posterous.new(666, 'password')
+      Posterous.new(666, @p)
     end
   end
   
   def test_site_id_is_false
-    actual = Posterous.new('email_address', 'password')
+    actual = Posterous.new(@e, @p)
     assert_equal false, actual.site_id
   end
   
   def test_site_id_is_set
-    actual = Posterous.new('email_address', 'password', '174966')
+    actual = Posterous.new(@e, @p, '174966')
     assert_equal '174966', actual.site_id
   end
   
   def test_site_id_is_converted_to_string
-    actual = Posterous.new('email_address', 'password', 174966)
+    actual = Posterous.new(@e, @p, 174966)
     assert_equal '174966', actual.site_id
   end  
 
   def test_user_authentication_success
-    post = Posterous.new('email_address', 'password')
-    post.stubs(:ping_account).returns(@good_response)
-    assert_equal true, post.valid_user?
+    @new_obj.stubs(:ping_account).returns(@good_response)
+    assert_equal true, @new_obj.valid_user?
   end
   
   def test_user_authentication_fail
-    post = Posterous.new('bad_email', 'password')
-    post.stubs(:ping_account).returns(@bad_response)
-    assert_equal false, post.valid_user?
+    @new_obj.stubs(:ping_account).returns(@bad_response)
+    assert_equal false, @new_obj.valid_user?
   end
 
-  def test_ping_authentication_success
+  def test_ping_authentication_success_hash
     Posterous.stubs(:post).returns(@resp_ok)
-    assert_equal @good_response, Posterous.new('e', 'p').ping_account
-    assert_not_equal @resp_ok, @good_response
+    assert_equal @good_response, @new_obj.ping_account
   end
   
-  def test_ping_authentication_fail
+  def test_ping_authentication_fail_hash
     Posterous.stubs(:post).returns(@resp_fail)
-    assert_equal @bad_response, Posterous.new('jordandobson@gmail.com', 'password').ping_account
-    assert_not_equal @resp_fail, @bad_response
+    assert_equal @bad_response, @new_obj.ping_account
   end
 
 end
